e7c0981391f0d2a84fd97783b186b9e5b79a98df
Cuong <cuong.lt204637@sis.hust.edu,vn>
2023-04-28 22:08:14 +0700

    Signup Form

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,135 +1,118 @@
-import React from 'react'
-import './Auth.css'
-const handle_slide_login = (event)=>{
-  event.preventDefault();
-  const form_login_element = document.getElementsByClassName("form_login")[0];
-  const form_signup_element = document.getElementsByClassName("form_signup")[0];
-  const title_login_element = document.getElementsByClassName("title_login")[0];
-  const title_signup_element = document.getElementsByClassName("title_signup")[0];
-  const slide_control_element = document.getElementsByClassName("slide_control")[0];
-  const label_login = slide_control_element.getElementsByTagName("label")[0];
-  const label_signup = slide_control_element.getElementsByTagName("label")[1];
-  form_login_element.style.display = "block";
-  form_signup_element.style.display = "none";
-  title_login_element.style.display = "block";
-  title_signup_element.style.display = "none";
-  label_login.style.backgroundColor = "rgb(190, 176, 44)";
-  label_signup.style.backgroundColor = "white";
-  label_signup.style.color = "black";
-  label_login.style.color = "white";
+import React from "react"
+import "./Auth.css"
+
+const Login_showing = (event)=>{
+  const title = document.querySelectorAll(".title div");
+  const form = document.querySelectorAll(".Form4 form");
+  title[0].style.display ="block";
+  title[1].style.display ="none";
+  form[0].style.display = "block";
+  form[1].style.display = "none";
 }
-const handle_slide_signup = (event)=>{
+const Signup_showing = (event)=>{
   event.preventDefault();
-  const form_login_element = document.getElementsByClassName("form_login")[0];
-  const form_signup_element = document.getElementsByClassName("form_signup")[0];
-  const title_login_element = document.getElementsByClassName("title_login")[0];
-  const title_signup_element = document.getElementsByClassName("title_signup")[0];
-  const slide_control_element = document.getElementsByClassName("slide_control")[0];
-  const label_login = slide_control_element.getElementsByTagName("label")[0];
-  const label_signup = slide_control_element.getElementsByTagName("label")[1];
-  form_login_element.style.display = "none";
-  form_signup_element.style.display = "block";
-  
-  title_login_element.style.display = "none";
-  title_signup_element.style.display = "block";
-  label_signup.style.backgroundColor = "rgb(190, 176, 44)";
-  label_login.style.backgroundColor = "white";
-  label_login.style.color = "black";
-  label_signup.style.color = "white"
+  const title = document.querySelectorAll(".title div");
+  const form = document.querySelectorAll(".Form4 form");
+  title[0].style.display ="none";
+  title[1].style.display ="block";
+  form[0].style.display = "none";
+  form[1].style.display = "block";
 }
-const handle_login = (event)=>{
+
+const Login = (event)=>{
   event.preventDefault();
-  
-  const {user_phone,user_password} = event.target;
+  const {account, password} = event.target;
   const payload  = {
-    phone: user_phone.value,
-    password: user_password.value
-  }
-  console.log(payload);
-}
-const handle_signup = (event)=>{
-  event.preventDefault();
-  const {user_phone, user_password,user_confirm_password,user_name,user_email,user_birthday} = event.target;
-  const payload = {
-    phone: user_phone.value,
-    password: user_password.value,
-    name: user_name.value,
-    email: user_email.value,
-    birthday: user_birthday.value
+    account:  account.value,
+    password: password.value
   }
   console.log(payload);
 }
 
-const Auth = () => {
+const Signup = (event)=>{
+  event.preventDefault();
+  const {account, password, confirm , name, email, birthday} = event.target;
+  const payload = {
+    account: account.value,
+    password: password.value,
+    confirm_password: confirm.value,
+    name: name.value,
+    email:  email.value,
+    birthday: birthday.value
+  }
+  console.log(payload);
+}
+
+const main = () => {
   return (
-    <div className='Auth'>
-      <div className="wrapper">
-        
-          <div className="title_text">
-            <h1 className='title_login'>Login Form</h1>
-            <h1 className='title_signup'>SignUp Form</h1>
+
+    <div className="Form">
+      <div className="Form1">
+      
+          <div className="title">
+            <div className="title_login">Login Form</div>
+            <div className="title_signup">Signup Form</div>
           </div>
-          <div className="form_container">
-            <div className="slide_control">
-              <input type="radio" id='login' name='slide' />
-              <input type="radio" id='signup' name='slide' />
-              <label htmlFor="login" className="slide login" onClick={handle_slide_login}>
+
+          <div className="Form3">
+            <div className="slide">
+              <label htmlFor="login" className="slide_login" onClick={Login_showing}>
                 Login
               </label>
-              <label htmlFor="signup" className='slide signup' onClick={handle_slide_signup}>
-                SignUp
+              <label htmlFor="signup" className="slide_signup" onClick={Signup_showing}>
+                Signup
               </label>
             </div>
-            <div className="form_inner">
-            <form action="" className='form_login' onSubmit={handle_login}>
-              <div className="field">
-                <input type="text" id='user_phone' placeholder='Phone' required />
-              </div>
-              <div className="field">
-                <input type="password" id='user_password' placeholder='Password' required />
-              </div>
-              <div className="pass_link">
-                <a href="facebook.com">Forgot password?</a>
-              </div>
-              <div className="field_btn">
-                <input type="submit" value = "Log In" id = "Login_btn" />
-              </div>
-              <div className="signup_link">
-                <p>Not a member?<a href='facebook.com' onClick={handle_slide_signup}>&nbsp;Signup now</a></p>
-              </div>
-            </form>
-            <form action="" className='form_signup' onSubmit={handle_signup}>
-              <div className="field">
-                <input type="text" id='user_phone' placeholder='Phone' required/>
-              </div>
-              <div className="field">
-                <input type="password" id='user_password' placeholder='Password' required/>
-              </div>
-              <div className="field">
-                <input type="password" id='user_confirm_password' placeholder='Confirm Password' required/>
-              </div>
-              <div className="field">
-                <input type="text" id='user_name' placeholder='Name' required/>
-              </div>
-              <div className="field">
-                <input type="text" id='user_email' placeholder='Email' required/>
-              </div>
-              <div className="field">
-                <input type="date" id='user_birthday' placeholder='Birthday' required/>
-              </div>
-              <div className="field_btn">
-                <input type="submit" value = "Sign Up" id = "signup_btn" />
-              </div>
-              
-            </form>
+
+            <div className="Form4">
+              <form action="" className="form_login" onSubmit={Login}>
+                <div className="field">
+                  <input type="text" id="account" placeholder="Account" required />
+                </div>
+                <div className="field">
+                  <input type="password" id="password" placeholder="Password" required />
+                </div>
+                <div className="forgot_link">
+                  <a href="">Forgot password?</a>
+                </div>
+                <div className="submit">
+                  <input type="submit" value = "Log in" id="submit"/>
+                </div>
+                <div className="signup_link">
+                    Not a member?<a href="" onClick={Signup_showing}>&nbsp;Signup now</a>
+                </div>
+              </form>
+
+              <form action="" className="form_signup" onSubmit={Signup}>
+                <div className="field">
+                  <input type="text" id="account" placeholder="Account" required/>
+                </div>
+                <div className="field">
+                  <input type="password" id="password" placeholder="Password" required/>
+                </div>
+                <div className="field">
+                  <input type="password" id="confirm" placeholder="Confirm Password" required/>
+                </div>
+                <div className="field">
+                  <input type="text" id="name" placeholder="Name" required/>
+                </div>
+                <div className="field">
+                  <input type="text" id="email" placeholder="Email" required/>
+                </div>
+                <div className="field">
+                  <input type="date" id="birthday" required/>
+                </div>
+                <div className="submit">
+                  <input type="submit" value = "Sign up"/>
+                </div>
+              </form>
+
             </div>
-            
-          
         </div>  
-         
       </div>
     </div>
+
   )
 }
 
-export default Auth
+export default main
