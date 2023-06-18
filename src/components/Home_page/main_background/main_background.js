3fdb0ccce6e832809424c59a130da87b1eb5aaf4
Cuong <cuong.lt204637@sis.hust.edu,vn>
2023-06-18 09:28:30 +0700

    fix

--- src/components/Home_page/main_background/main_background.js
+++ src/components/Home_page/main_background/main_background.js
@@ -6,16 +6,18 @@ import Workspace from "./workspace/workspace";
 import ProjectScreen from "./project/Project";
 import TodayScreen from "./today/TodayUser";
 import { useSelector } from "react-redux";
+
 const MainBackground = () => {
-    const user = useSelector((state) => state.user?.userData);
+    // const user = useSelector((state) => state.user?.userData);
     return (
         <div className="main_container">
-            <div></div>
-            <Routes>
-                <Route path="today" element={<TodayScreen />} />
-                <Route path="assignment" element={<Workspace />} />
-                <Route path="project" element={<ProjectScreen />} />
-            </Routes>
+            <div>
+                <Routes>
+                    <Route path='today' element={<TodayScreen />} />
+                    <Route path="assignment" element={<Workspace />} />
+                    <Route path="project" element={<ProjectScreen />} />
+                </Routes>
+            </div>
         </div>
     );
 };
